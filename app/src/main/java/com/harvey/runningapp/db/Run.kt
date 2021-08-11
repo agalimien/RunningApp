28ec9ab855b41ec88ddc8d030fd63977081057b8
Harvey Tai <40065121+agalimien@users.noreply.github.com>
2021-08-11 17:16:16 +0800

    RunEntityAndDAO Keep going

---
+++ app/src/main/java/com/harvey/runningapp/db/Run.kt
@@ -0,0 +1,16 @@
+package com.harvey.runningapp.db
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "running_table")
+data class Run(
+    @PrimaryKey(autoGenerate = true) var id: Int? = null,
+    var img: Bitmap? = null,
+    var timestamp: Long = 0L,
+    var avgSpeedInKMH: Float = 0f,
+    var distanceInMeters: Int = 0,
+    var timeInMillis: Long = 0L,
+    var caloriesBurned: Int = 0
+)
