1860dfa8c494da46c47d34e141c921feec86f990
harvey.tai <harvey.tai@twprotech.com>
2021-08-11 22:41:43 +0800

    RunningEntityAndDAO ok

--- app/src/main/java/com/harvey/runningapp/db/RunDAO.kt
+++ app/src/main/java/com/harvey/runningapp/db/RunDAO.kt
@@ -27,6 +27,15 @@ interface RunDAO {
     @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
     fun getAllRunsSortedByDistance(): LiveData<List<Run>>
 
-    
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
+    fun getTotalTimeInMillis(): LiveData<Long>
 
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCaloriesBurned(): LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance(): LiveData<Int>
+
+    @Query("SELECT AVG(avgSpeedInKMH) FROM running_table")
+    fun getTotalAvgSpeed(): LiveData<Float>
 }
