34dc5a430af2390a4a584da38b7b4e94956b95f8
Harvey Tai <40065121+agalimien@users.noreply.github.com>
2021-08-13 13:35:09 +0800

    HandlingLocationPermissions ok

--- app/src/main/java/com/harvey/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/harvey/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,13 +1,21 @@
 package com.harvey.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.harvey.runningapp.R
 import com.harvey.runningapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
 class TrackingFragment: Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+    }
 }
