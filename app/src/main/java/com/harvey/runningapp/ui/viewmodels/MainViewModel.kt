f9254e07dfed93e9e4c200671824120f687715cc
harvey.tai <harvey.tai@twprotech.com>
2021-08-19 23:00:22 +0800

    SortingRuns MainViewModel

--- app/src/main/java/com/harvey/runningapp/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/harvey/runningapp/ui/viewmodels/MainViewModel.kt
@@ -1,8 +1,10 @@
 package com.harvey.runningapp.ui.viewmodels
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.harvey.runningapp.db.Run
+import com.harvey.runningapp.other.SortType
 import com.harvey.runningapp.repositories.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,7 +15,23 @@ class MainViewModel @Inject constructor(
     val mainRepository: MainRepository
 ) : ViewModel() {
 
-    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+    private val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+    private val runsSortedByDistance = mainRepository.getAllRunsSortedByDistance()
+    private val runsSortedByCaloriesBurned = mainRepository.getAllRunsSortedByCaloriesBurned()
+    private val runsSortedByTimeInMillis = mainRepository.getAllRunsSortedByTimeInMillis()
+    private val runsSortedByAvgSpeed = mainRepository.getAllRunsSortedByAvgSpeed()
+
+    val runs = MediatorLiveData<List<Run>>()
+
+    var sortType = SortType.DATE
+
+    init {
+        runs.addSource(runsSortedByDate) { result ->
+            if (sortType == SortType.DATE) {
+                result?.let { runs.value = it }
+            }
+        }
+    }
 
     fun insertRun(run: Run) = viewModelScope.launch {
         mainRepository.insertRun(run)
