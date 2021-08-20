1de0878f6dbd5fc7e5fd01562270de19b493b907
Harvey Tai <40065121+agalimien@users.noreply.github.com>
2021-08-20 15:28:52 +0800

    TotalRunStatistics keep going

--- app/src/main/java/com/harvey/runningapp/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/harvey/runningapp/ui/viewmodels/StatisticsViewModel.kt
@@ -9,4 +9,11 @@ import javax.inject.Inject
 class StatisticsViewModel @Inject constructor(
     val mainRepository: MainRepository
 ) : ViewModel() {
+
+    val totalRunTime = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+    val totalAvgSpeed = mainRepository.getTotalAvgSpeed()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
 }
